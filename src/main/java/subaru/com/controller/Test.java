09aeae205b0afaa90ac7f6dee53dc5a02dd126d0
laixiaohang <laixiaohang@sina.com>
2015-08-30 15:28:37 +0800

    S->s

--- src/main/java/subaru/com/controller/Test.java
+++ src/main/java/subaru/com/controller/Test.java
@@ -1,8 +1,9 @@
-package Subaru.com.Controller;
+package subaru.com.controller;
 
 public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
+		
 	}
 }
