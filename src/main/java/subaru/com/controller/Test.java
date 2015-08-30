c402344cb4834867fd16445f67d7d624d288274f
laixiaohang <laixiaohang@sina.com>
2015-08-30 16:02:46 +0800

    test

--- src/main/java/subaru/com/controller/Test.java
+++ src/main/java/subaru/com/controller/Test.java
@@ -3,7 +3,7 @@ package subaru.com.controller;
 public class Test {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println("Hello cloud!");
 		
 	}
 }
